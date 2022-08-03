3a1d2434c0620df475ea31f6e1ec172a67c7a7af
David Basto <davidbasto01@gmail.com>
2022-08-03 14:07:06 -0500

    initial config

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+  },
+  root: true,
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-prettier'],
+  rules: {
+    semi: ['error', 'always'],
+  },
+};
