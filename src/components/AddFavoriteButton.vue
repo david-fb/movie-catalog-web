2d3218329aad9a7b486a197da0b5c8553bc72b6b
David Basto <davidbasto01@gmail.com>
2022-08-04 21:32:56 -0500

    fix: click propagation

--- src/components/AddFavoriteButton.vue
+++ src/components/AddFavoriteButton.vue
@@ -4,7 +4,7 @@ function addFavotire(){
 }
 </script>
 <template>
-<svg version="1.1" id="Layer_1" x="0px" y="0px" @click="addFavotire"
+<svg version="1.1" id="Layer_1" x="0px" y="0px" @click.stop="addFavotire"
 	 viewBox="0 0 476.213 476.213" style="enable-background:new 0 0 476.213 476.213;" fill="#f6c434" xml:space="preserve">
 <polygon points="36.213,461.213 461.213,461.213 461.213,36.213"/>
 <path d="M476.213,476.213H0L476.213,0V476.213z M72.427,446.213h373.787V72.427L72.427,446.213z"/>
